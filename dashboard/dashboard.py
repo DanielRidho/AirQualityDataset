92c91bde166fa1ed702566cbc015b757cc98b848
DanielRidho <164577812+DanielRidho@users.noreply.github.com>
2024-10-24 05:21:15 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,12 +4,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
-import os
 
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv(os.path.join(os.getcwd(), 'clean_merged_dataset.csv'))
+    df = pd.read_csv("../dashboard/clean_merged_dataset.csv")
     df_Aotizhongxin = pd.read_csv(
         "../data/PRSA_Data_Aotizhongxin_20130301-20170228.csv"
     )
