6c1692ff140660e5fff0ee7c2d62fa15d0a686c9
DanielRidho <164577812+DanielRidho@users.noreply.github.com>
2024-10-30 20:05:43 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -49,7 +49,7 @@ def load_data():
 
     #df["date_time"] = pd.to_datetime(df["date_time"])
     #df["date_time"] = pd.to_datetime(df["date_time"])
-    df["date_time"] = pd.to_datetime(df["date_time"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+    df["date_time"] = pd.to_datetime(df["date_time"])
 
     return (
         df,
@@ -165,7 +165,7 @@ elif menu == "Pertanyaan 1":
 
     # Mengonversi kolom date_time menjadi datetime
     #df["date_time"] = pd.to_datetime(df["date_time"])
-    df["date_time"] = pd.to_datetime(df["date_time"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+    df["date_time"] = pd.to_datetime(df["date_time"])
 
     # Membuat plot
     yearly_co = df.groupby('year')['CO'].mean().reset_index()
@@ -214,7 +214,7 @@ elif menu == "Pertanyaan 2":
 
     # Mengonversi kolom date_time menjadi datetime
     #df["date_time"] = pd.to_datetime(df["date_time"])
-    df["date_time"] = pd.to_datetime(df["date_time"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+    df["date_time"] = pd.to_datetime(df["date_time"])
     
     # Membuat scatter plot
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -254,7 +254,7 @@ elif menu == "Pertanyaan 3":
 
     # Mengonversi kolom date_time menjadi datetime
     #df["date_time"] = pd.to_datetime(df["date_time"])
-    df["date_time"] = pd.to_datetime(df["date_time"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+    df["date_time"] = pd.to_datetime(df["date_time"])
 
     # Menambahkan plot
     def get_season(month):
